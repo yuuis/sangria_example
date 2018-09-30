bb6c265017e0e57763950daf7ee6ff1532d8b103
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-01 01:37:01 +0900

    add to more tags

--- src/main/scala/repository/ArticleRepository.scala
+++ src/main/scala/repository/ArticleRepository.scala
@@ -17,17 +17,17 @@ class ArticleRepository {
 object ArticleRepository {
   val articles = List(
     Article("1", "about AWS", Some("1"), List("aws", "infla")),
-    Article("2", "about scala", Some("2"), List("null", "unforgivable")),
-    Article("3", "about scala_2", Some("2"), List("null", "unforgivable")),
-    Article("4", "about scala_3", Some("2"), List("null", "unforgivable")),
-    Article("5", "about TaPL", Some("3"), List()),
-    Article("6", "I found dynamic typing lanuguage is awesome", Some("4"), List()),
-    Article("7", "I came back static typing language", Some("4"), List()),
-    Article("8", "about Haskell", Some("5"), List()),
-    Article("9", "about Haskell_2", Some("5"), List()),
-    Article("10", "about F#", Some("6"), List()),
+    Article("2", "about scala", Some("2"), List("scala", "functional", "oop")),
+    Article("3", "about scala_2", Some("2"), List("scala", "functional", "oop")),
+    Article("4", "about scala_3", Some("2"), List("scala", "functional", "oop")),
+    Article("5", "about TaPL", Some("3"), List("TaPL")),
+    Article("6", "I found dynamic typing lanuguage is awesome", Some("4"), List("ruby")),
+    Article("7", "I came back static typing language", Some("4"), List("scala", "Haskell")),
+    Article("8", "about Haskell", Some("5"), List("Haskell", "functional")),
+    Article("9", "about Haskell_2", Some("5"), List("Haskell", "functional")),
+    Article("10", "about F#", Some("6"), List("F#")),
     Article("11", "rejected", None, List()),
-    Article("12", "about Typescript", Some("7"), List()),
-    Article("13", "about Typescript2", Some("7"), List()),
+    Article("12", "about Typescript", Some("7"), List("js", "ts", "front-end")),
+    Article("13", "about Typescript2", Some("7"), List("js", "ts", "front-end")),
     Article("14", "rejected", None, List()))
 }
