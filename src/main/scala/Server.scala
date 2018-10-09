92aa8487ffc5508efd96a27e097f3e85ad9bc8a8
Yu Ishiguro <yuuis.egr@gmail.com>
2018-10-09 18:41:09 +0900

    change host

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -3,5 +3,5 @@ import http.Routes
 
 object Server extends App with Routes{
 
-  Http().bindAndHandle(route, "0.0.0.0", sys.props.get("http.port").fold(9000)(_.toInt))
+  Http().bindAndHandle(route, "localhost", sys.props.get("http.port").fold(9000)(_.toInt))
 }
